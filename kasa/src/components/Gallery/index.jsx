cf771092ed15050142e85da6128178bf766be978
Florence <loison.florence@gmail.com>
2024-10-11 21:47:52 +0200

    Ajout key sur le <Link>

--- kasa/src/components/Gallery/index.jsx
+++ kasa/src/components/Gallery/index.jsx
@@ -2,54 +2,26 @@ import "../../styles/index.scss";
 import logements from "../../logements.json";
 import { Link } from "react-router-dom";
 
-
 function Gallery() {
   return (
     <div className="gallery">
       {logements.map((logement) => (
-        <Link className="gallery__link" to={`/logement/${logement.id}`}>
-        <div className="gallery__card" key={logement.id}>
-       {/* <div className="gallery__essai"> */}
-          <img
-            className="gallery__img"
-            src={logement.cover}
-            alt={logement.title}
-          />
-          <h2 className="gallery__titleimg">{logement.title}</h2>
-          
-        {/* </div> */}
-        </div>
+        <Link className="gallery__link" to={`/logement/${logement.id}`} key={logement.id}>
+          <div className="gallery__card">
+            {/* <div className="gallery__essai"> */}
+            <img
+              className="gallery__img"
+              src={logement.cover}
+              alt={logement.title}
+            />
+            <h2 className="gallery__titleimg">{logement.title}</h2>
+
+            {/* </div> */}
+          </div>
         </Link>
-      
       ))}
-      
     </div>
   );
 }
 
 export default Gallery;
-
-// function Renvoie (){
-//   return (
-//     <div className="gal">
-//     {logements.map((description) => (
-//       <div className="gallery__Kard" key={description.pictures []}>
-//         {/* <Link to ={`/logement/${location}`}> */}
-//         <img
-//           className="galleri__img"
-//           src={pictures}
-//           alt={photos logement}
-//         />
-//         <h2 className="galleri__titleimg">{host}</h2>
-//       </div>
-//         <h2 className="galleri__titleimg">{rating}</h2>
-//       </div>
-//     ))}
-//     {/* // <Link to="/">Accueil</Link> */}
-//   )
-// export {Renvoie}
-// // {/* <Link to="/">Accueil</Link>
-// //         </p>
-// //         <p className="site-header__text buttonabout">
-// //         <Link to="/about">A propos </Link>
-// //         </p> */}
