a261d87f63f2dd2a5bb7bbc1db262583abf07b06
Florence <loison.florence@gmail.com>
2024-10-11 21:48:28 +0200

    Correction CSS

--- kasa/src/pages/About/index.jsx
+++ kasa/src/pages/About/index.jsx
@@ -9,48 +9,46 @@ import BannerImg2 from "../../images/bannerpagescollapses.png";
 
 const About = () => {
   return (
-    <div>
-      <div className="about">
-        <Banner
-          bannerImg={BannerImg2}
-          altImg="Montagnes"
-          opacityDark="30%"
-          slogan=""
-        />
+    <div className="about">
+      <Banner
+        bannerImg={BannerImg2}
+        altImg="Montagnes"
+        opacityDark="30%"
+        slogan=""
+      />
 
-        <div className="aboutdescription">
-          <Collapses
-            title="Flexibilité"
-            description="Les annonces postées sur Kasa garantissent une fiabilité totale. Les
+      <div className="about__description">
+        <Collapses
+          title="Flexibilité"
+          description="Les annonces postées sur Kasa garantissent une fiabilité totale. Les
           photos sont conformes aux logements, et toutes les informations sont
            régulierement vérifiées par nos équipes."
-          />
+        />
 
-          <Collapses
-            title="Respect"
-            description="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
+        <Collapses
+          title="Respect"
+          description="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
             comportement discriminatoire ou de perturbation du voisinage
             entraînera une exclusion de notre plateforme."
-          />
+        />
 
-          <Collapses
-            title="Service"
-            description="La qualité du service est au coeur de notre engagement chez Kasa.
+        <Collapses
+          title="Service"
+          description="La qualité du service est au coeur de notre engagement chez Kasa.
             Nous veillons à ce que chaque interaction, que ce soit avec nos
             hôtes ou nos locataires, soit empreinte de respect et de
             bienveillance."
-          />
+        />
 
-          <Collapses
-            title="Securité"
-            description="La securité est la priorité de Kasa. Aussi bien pour nos hôtes que
+        <Collapses
+          title="Securité"
+          description="La securité est la priorité de Kasa. Aussi bien pour nos hôtes que
             pour les voyageurs, chaque logement correspond aux critéres de
             securité de établis par nos services. En laissant une note aussi
             bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier
             que les standards sont bien respectés. Nous organisons également des
             ateliers sur la sécurité domestique pour nos hôtes."
-          />
-        </div>
+        />
       </div>
     </div>
   );
