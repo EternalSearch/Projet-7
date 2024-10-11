d8b63a67369fe0e4895bbe9f4b43c1bd79392fd0
Florence <loison.florence@gmail.com>
2024-10-11 21:05:13 +0200

    Intégration composant Banner

--- kasa/src/pages/About/index.jsx
+++ kasa/src/pages/About/index.jsx
@@ -4,44 +4,55 @@ import BannerImg from "../../images/bannerpagescollapses.png";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { faChevronUp, faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import Collapses from "../../components/Collapses";
-import SectionBanner from "../../components/SectionBanner";
+import Banner from "../../components/Banner";
+import BannerImg2 from "../../images/bannerpagescollapses.png";
 
 const About = () => {
-
-
   return (
     <div>
       <div className="about">
+        <Banner
+          bannerImg={BannerImg2}
+          altImg="Montagnes"
+          opacityDark="30%"
+          slogan=""
+        />
 
-        {/* <SectionBanner /> */}
-      
-        <img className="banner__img" src={BannerImg} alt="Montagnes" />   
-
-     
-      <div className="aboutdescription">
-        <Collapses title="Flexibilité" description="Les annonces postées sur Kasa garantissent une fiabilité totale. Les
+        <div className="aboutdescription">
+          <Collapses
+            title="Flexibilité"
+            description="Les annonces postées sur Kasa garantissent une fiabilité totale. Les
           photos sont conformes aux logements, et toutes les informations sont
-           régulierement vérifiées par nos équipes."/>
+           régulierement vérifiées par nos équipes."
+          />
 
-        <Collapses title="Respect" description="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
+          <Collapses
+            title="Respect"
+            description="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
             comportement discriminatoire ou de perturbation du voisinage
-            entraînera une exclusion de notre plateforme."/>
+            entraînera une exclusion de notre plateforme."
+          />
 
-        <Collapses title="Service" description="La qualité du service est au coeur de notre engagement chez Kasa.
+          <Collapses
+            title="Service"
+            description="La qualité du service est au coeur de notre engagement chez Kasa.
             Nous veillons à ce que chaque interaction, que ce soit avec nos
             hôtes ou nos locataires, soit empreinte de respect et de
-            bienveillance."/>
+            bienveillance."
+          />
 
-        <Collapses title="Securité" description="La securité est la priorité de Kasa. Aussi bien pour nos hôtes que
+          <Collapses
+            title="Securité"
+            description="La securité est la priorité de Kasa. Aussi bien pour nos hôtes que
             pour les voyageurs, chaque logement correspond aux critéres de
             securité de établis par nos services. En laissant une note aussi
             bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier
             que les standards sont bien respectés. Nous organisons également des
-            ateliers sur la sécurité domestique pour nos hôtes."/>
+            ateliers sur la sécurité domestique pour nos hôtes."
+          />
+        </div>
+      </div>
     </div>
-      </div>
-      </div>
-      
-
-);}
-export default About
+  );
+};
+export default About;
--- kasa/src/pages/HomePage/index.jsx
+++ kasa/src/pages/HomePage/index.jsx
@@ -1,17 +1,19 @@
-
-import SectionBanner from '../../components/SectionBanner';
-import Gallery from '../../components/Gallery';
+import Banner from "../../components/Banner";
+import Gallery from "../../components/Gallery";
+import ImgBanner from "../../images/Imagesource1.png";
 import "../../styles/index.scss";
 
 const HomePage = () => {
-    return (
-        <div className="home">
-           
-            <SectionBanner />
-            <Gallery />
-           
-        </div>
-    );
+  return (
+    <div className="home">
+      <Banner
+        bannerImg={ImgBanner}
+        altImg="Rochers"
+        opacityDark="60%"
+        slogan="Chez vous, partout et ailleurs"
+      />
+      <Gallery />
+    </div>
+  );
 };
 export default HomePage;
-
